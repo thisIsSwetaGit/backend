597cc0ec0d4e627ee557ab44d0dc94e37fa04908
Sweta Dutta <162873740+thisIsSwetaGit@users.noreply.github.com>
2025-07-11 05:21:07 +0000

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,44 @@
+// require("dotenv").config({path:"./env"});
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+dotenv.config({ 
+  path: "./env"
+});
+
+
+connectDB()
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+;(async () => {
+    try {
+      await  mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+      app.on('error', (error) => {
+        console.error("Err",error);
+        throw error;
+      })
+      app.listen(process.env.PORT, () => {
+        console.log(`app is running on port ${process.env.PORT}`);
+      })
+    }
+    catch (error) {
+        console.error("Error:", error);
+        throw err;
+    }
+})()
+    */
